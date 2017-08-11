5cb6797af0c1b1e7915196436eb21af16fc9ff26
Purushottam Kumar <purushottam@gosharp.in>
2017-08-11 11:46:22 +0530

    MBProgressHUD implementation done.

--- MBProgressHud/ViewController.m
+++ MBProgressHud/ViewController.m
@@ -1,29 +1,69 @@
-//
-//  ViewController.m
-//  MBProgressHud
-//
-//  Created by Purushottam Kumar on 09/08/17.
-//  Copyright © 2017 Purushottam Kumar. All rights reserved.
-//
+
+/*  Please follow the following step to implement the hud in any projects:-
+ 
+1. First copy MBProgressHUD.h and MBProgressHUD.m class from this project
+2. Next add the MBProgressHUD.h and MBProgressHUD.m file into a new project
+3. Next #import "MBProgressHUD.h" the class name where you want to use the hud
+ 
+*/
 
 #import "ViewController.h"
-
+#import "MBProgressHUD.h"
+#import <QuartzCore/QuartzCore.h>
 @interface ViewController ()
+{
+    MBProgressHUD *hud;
+    UIProgressView *progressView;
+
+   
+}
+- (IBAction)showHudButtonAction:(id)sender;
+- (IBAction)hideHudButtonAction:(id)sender;
+
+@property (nonatomic, assign) SEL selector;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
+
+// Show Hud button action
+- (IBAction)showHudButtonAction:(id)sender
+{
+    hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+
+    hud.labelColor=[UIColor whiteColor];
+    hud.square = YES;
+    
+    // Hud label message
+    hud.labelText= @"Wait";
+    hud.detailsLabelText = @"Processing....";
+    hud.mode = MBProgressHUDModeIndeterminate;
+  
+    // Transparent background color
+    hud.backgroundColor= [[UIColor blackColor] colorWithAlphaComponent:0.8];
+    hud.labelFont = [UIFont fontWithName:@"MuseoSans-300.otf" size:50];
+    hud.color =[UIColor clearColor];
+    [UIColor colorWithRed:255 green:.32 blue:.08 alpha:1];
+    hud.userInteractionEnabled = NO;
+}
+
+// Hide Hud button action
+- (IBAction)hideHudButtonAction:(id)sender
+{
+    [hud hide:YES];
+}
+
 
 
 @end
